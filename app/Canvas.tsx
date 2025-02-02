a8c91869511f6cfff61d7b5080eb9944f0a40294
Alex <alexanderthestudent@yandex.ru>
2025-02-02 16:00:00 +0300

    feat: move & resize

--- app/Canvas.tsx
+++ app/Canvas.tsx
@@ -12,15 +12,16 @@ export default function Canvas({slide, setSlide}: {slide: Slide, setSlide: (upda
     }
 
     const box = ref.current.getBoundingClientRect();
+    const oldGeo = target.geometry;
 
     const update: SlideObject = {
       ...target,
       geometry: {
-        left: type === "move" ? x / box.width * 100 : target.geometry.left,
-        top: type === "move" ? y / box.height * 100 : target.geometry.top,
-        width: type === "resize" ? x / box.width * 100 : target.geometry.width,
-        height: type === "resize" ? y / box.height * 100 : target.geometry.height,
-        borderRadius: target.geometry.borderRadius,
+        left: type === "move" ? x / box.width * 100 : oldGeo.left,
+        top: type === "move" ? y / box.height * 100 : oldGeo.top,
+        width: type === "resize" ? x / box.width * 100 - oldGeo.left : oldGeo.width,
+        height: type === "resize" ? y / box.height * 100 - oldGeo.top : oldGeo.height,
+        borderRadius: oldGeo.borderRadius,
       },
     }
 
@@ -28,7 +29,7 @@ export default function Canvas({slide, setSlide}: {slide: Slide, setSlide: (upda
     setSlide({...slide, objects: [...rest, update]})
   }
   
-  return <div ref={ref} className="h-full">
+  return <div ref={ref} className="h-full relative">
     {slide.objects.map(object => (
       <ObjectComponent
         {...object}
@@ -42,12 +43,12 @@ export function ObjectComponent(props: SlideObject & {onMouse: (type: 'move' | '
   const geometryEntries = Object.entries(props.geometry).map(([k, v]) => [k, `${v}%`])
   const geometry = Object.fromEntries(geometryEntries);
 
-  function startTrackingMove() {
+  function startTracking(type: "move" | "resize") {
     const controller = new AbortController();
     window.addEventListener('mouseup', () => controller.abort());
 
     window.addEventListener('mousemove', (event) => {
-      props.onMouse("move", event.clientX, event.clientY)
+      props.onMouse(type, event.clientX, event.clientY)
     }, {signal: controller.signal})
   }
 
@@ -65,10 +66,10 @@ export function ObjectComponent(props: SlideObject & {onMouse: (type: 'move' | '
   }
 
   return <div style={geometry} className="absolute bg-surface0 p-2">
-    <button className="absolute w-16 h-16 bg-blue rounded-full flex justify-center items-center hover:bg-teal transition" style={{top: 0, left: 0, transform: "translate(-50%, -50%)"}}>
-      <ViewfinderCircleIcon className="text-base rotate-45 size-8" onMouseDown={startTrackingMove} />
+    <button onMouseDown={() => startTracking("move")} className="absolute w-16 h-16 bg-blue rounded-full flex justify-center items-center hover:bg-teal transition" style={{top: 0, left: 0, transform: "translate(-50%, -50%)"}}>
+      <ViewfinderCircleIcon className="text-base rotate-45 size-8" />
     </button>
-    <button className="absolute w-16 h-16 bg-green rounded-full flex justify-center items-center hover:bg-teal transition" style={{bottom: 0, right: 0, transform: "translate(50%, 50%)"}}>
+    <button onMouseDown={() => startTracking("resize")} className="absolute w-16 h-16 bg-green rounded-full flex justify-center items-center hover:bg-teal transition" style={{bottom: 0, right: 0, transform: "translate(50%, 50%)"}}>
       <ChevronUpDownIcon className="text-base -rotate-45 size-8" />
     </button>
     {body}
