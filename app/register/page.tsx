4f25df384ffbd66d79f180087147642d718a2414
Alex <alexanderthestudent@yandex.ru>
2025-02-24 19:07:25 +0300

    fix(auth): make the register page visually consistent with the rest of the app

--- app/register/page.tsx
+++ app/register/page.tsx
@@ -1,5 +1,7 @@
 "use client";
 
+import { CheckIcon, UserCircleIcon } from "@heroicons/react/16/solid";
+import Link from "next/link";
 import { redirect } from "next/navigation";
 import PocketBase from "pocketbase";
 import { FormEvent, useEffect } from "react";
@@ -28,43 +30,51 @@ export default function RegisterPage() {
   }
 
   return (
-    <div className="flex h-screen w-screen items-center justify-center bg-mantle">
-      <form className="grid w-80 gap-2 bg-base p-2" action={handleSubmit}>
-        <label>
-          Name
-          <input name="name" className="rounded-lg bg-mantle p-2" />
-        </label>
+    <div className="flex h-screen w-screen items-center justify-center">
+      <form className="flex flex-col gap-4" action={handleSubmit}>
+        <h1 className="text-center text-3xl text-blue underline">Register</h1>
 
-        <label>
-          E-mail
-          <input
-            name="email"
-            type="email"
-            className="rounded-lg bg-mantle p-2"
-          />
-        </label>
+        <input
+          name="name"
+          placeholder="Your Name"
+          className="block h-16 w-96 rounded-lg border-2 border-crust bg-base bg-mantle p-4 text-lg transition hover:border-sky focus:outline-none"
+        />
 
-        <label>
-          Password
-          <input
-            name="password"
-            type="password"
-            className="rounded-lg bg-mantle p-2"
-          />
-        </label>
+        <input
+          name="email"
+          type="email"
+          placeholder="E-Mail"
+          className="block h-16 w-96 rounded-lg border-2 border-crust bg-base bg-mantle p-4 text-lg transition hover:border-sky focus:outline-none"
+        />
 
-        <label>
-          Password Confirmation
-          <input
-            name="passwordConfirm"
-            type="password"
-            className="rounded-lg bg-mantle p-2"
-          />
-        </label>
+        <input
+          name="password"
+          type="password"
+          placeholder="Password"
+          className="block h-16 w-96 rounded-lg border-2 border-crust bg-base bg-mantle p-4 text-lg transition hover:border-sky focus:outline-none"
+        />
 
-        <button className="rounded-lg bg-blue p-2 text-base">Register</button>
-        <p>
-          Already have an account? <a href="/login">Log in</a>
+        <input
+          name="passwordConfirm"
+          type="password"
+          placeholder="Password (Confirmation)"
+          className="block h-16 w-96 rounded-lg border-2 border-crust bg-base bg-mantle p-4 text-lg transition hover:border-sky focus:outline-none"
+        />
+
+        <button className="flex h-16 w-full items-center justify-center rounded-lg border-2 border-crust bg-mantle text-base text-lg text-green transition hover:scale-95 hover:border-sky">
+          Confirm
+          <CheckIcon className="ml-2 size-8" />
+        </button>
+
+        <p className="group text-center text-subtext0">
+          <UserCircleIcon className="me-2 inline size-3 transition group-hover:text-sky" />
+          Already have an account?{" "}
+          <Link
+            href="/login"
+            className="group-hover:text-sky group-hover:underline"
+          >
+            Log In
+          </Link>
         </p>
       </form>
     </div>
